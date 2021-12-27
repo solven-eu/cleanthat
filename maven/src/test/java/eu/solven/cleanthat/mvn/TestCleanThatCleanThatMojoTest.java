4e98197a615be1a19cfc4c7a2400e0f41eeb3581
Benoit Lacelle <benoit.lacelle@gmail.com>
2021-12-27 18:02:08 +0100

    Fix tests

--- maven/src/test/java/eu/solven/cleanthat/mvn/TestCleanThatCleanThatMojoTest.java
+++ maven/src/test/java/eu/solven/cleanthat/mvn/TestCleanThatCleanThatMojoTest.java
@@ -1,18 +1,18 @@
 package eu.solven.cleanthat.mvn;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
 import org.apache.maven.project.MavenProject;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.boot.SpringApplication;
+
+import cormoran.pepper.unittest.ILogDisabler;
+import cormoran.pepper.unittest.PepperTestHelper;
 
 public class TestCleanThatCleanThatMojoTest extends ACleanThatMojoTest {
 	@Test
-	public void testCleanthat() throws Exception {
+	public void testCleanthat_noConfig() throws Exception {
 		String relativePathToParent = "/unit/project-to-test";
 		File readWriteFolder = temporaryFolder(relativePathToParent);
 
@@ -22,12 +22,35 @@ public class TestCleanThatCleanThatMojoTest extends ACleanThatMojoTest {
 
 		MavenProject project = prepareMojoInTemporaryFolder(relativePathToParent, readWriteFolder);
 
-		Resource defaultConfig = new ClassPathResource("/config/default-safe.yaml");
-		Files.copy(defaultConfig.getInputStream(), cleanthatYaml.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		// Resource defaultConfig = new ClassPathResource("/config/default-safe.yaml");
+		// Files.copy(defaultConfig.getInputStream(), cleanthatYaml.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
 		CleanThatCleanThatMojo myMojo = (CleanThatCleanThatMojo) lookupConfiguredMojo(project, "cleanthat");
-		assertNotNull(myMojo);
 
-		myMojo.execute();
+		try (ILogDisabler logCLoser = PepperTestHelper.disableLog(SpringApplication.class)) {
+			Assertions.assertThatThrownBy(() -> myMojo.execute()).isInstanceOf(IllegalStateException.class);
+		}
+	}
+
+	@Test
+	public void testCleanthat_initThenLint() throws Exception {
+		String relativePathToParent = "/unit/project-to-test";
+		File readWriteFolder = temporaryFolder(relativePathToParent);
+
+		// Ensure the test resources does not hold a cleanthat.yaml
+		File cleanthatYaml = new File(readWriteFolder, "cleanthat.yaml");
+		Assertions.assertThat(cleanthatYaml).doesNotExist();
+
+		MavenProject project = prepareMojoInTemporaryFolder(relativePathToParent, readWriteFolder);
+
+		// Resource defaultConfig = new ClassPathResource("/config/default-safe.yaml");
+		// Files.copy(defaultConfig.getInputStream(), cleanthatYaml.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+		// 'init' will create a cleanthat.yaml
+		CleanThatInitMojo initMojo = (CleanThatInitMojo) lookupConfiguredMojo(project, "init");
+		initMojo.execute();
+
+		CleanThatCleanThatMojo cleanthatMojo = (CleanThatCleanThatMojo) lookupConfiguredMojo(project, "cleanthat");
+		cleanthatMojo.execute();
 	}
 }

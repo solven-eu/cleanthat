3f9b17dd333e8c917e4a4ba59bd7fa02d932d30c
gbq6 <24852447+gbq6@users.noreply.github.com.>
2025-09-28 21:44:33 +0200

    feature: Implement AppendCharacterWithChar

---
+++ java/src/main/java/eu/solven/cleanthat/engine/java/refactorer/mutators/AppendCharacterWithChar.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2025 Benoit Lacelle - SOLVEN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.solven.cleanthat.engine.java.refactorer.mutators;
+
+import java.util.Optional;
+import java.util.Set;
+
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.expr.CharLiteralExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.utils.StringEscapeUtils;
+import com.google.common.collect.ImmutableSet;
+
+import eu.solven.cleanthat.engine.java.IJdkVersionConstants;
+import eu.solven.cleanthat.engine.java.refactorer.AJavaparserNodeMutator;
+import eu.solven.cleanthat.engine.java.refactorer.NodeAndSymbolSolver;
+
+/**
+ * Turns `stringBuilder.append("c")` into `stringBuilder.append('c')`
+ * <p>
+ * Works with classes listed in {@code SUPPORTED_TYPES}.
+ *
+ * @author Balazs Glatz
+ */
+public class AppendCharacterWithChar extends AJavaparserNodeMutator {
+
+	public static final String METHOD_APPEND = "append";
+
+	private static final Set<String> SUPPORTED_TYPES = Set
+			.of("java.io.CharArrayWriter", "java.io.StringWriter", "java.lang.StringBuffer", "java.lang.StringBuilder");
+
+	@Override
+	public String minimalJavaVersion() {
+		return IJdkVersionConstants.JDK_5;
+	}
+
+	@Override
+	public Set<String> getTags() {
+		return ImmutableSet.of("String");
+	}
+
+	@Override
+	public Optional<String> getPmdId() {
+		return Optional.of("AppendCharacterWithChar");
+	}
+
+	@Override
+	public String pmdUrl() {
+		return "https://pmd.github.io/pmd/pmd_rules_java_errorprone.html#appendcharacterwithchar";
+	}
+
+	@Override
+	protected boolean processNotRecursively(NodeAndSymbolSolver<?> node) {
+		if (!(node.getNode() instanceof MethodCallExpr)) {
+			return false;
+		}
+
+		var methodCall = (MethodCallExpr) node.getNode();
+		var methodName = methodCall.getNameAsString();
+		if (!METHOD_APPEND.equals(methodName) || methodCall.getArguments().size() != 1) {
+			return false;
+		}
+
+		Expression argument = methodCall.getArgument(0);
+		if (argument.isCharLiteralExpr() || !argument.isStringLiteralExpr()) {
+			return false;
+		}
+
+		Expression scope = methodCall.getScope().orElse(null);
+		if (!isSupportedType(scope)) {
+			return false;
+		}
+
+		var argumentAsString = argument.asStringLiteralExpr().getValue();
+		if (couldBeCharacter(argumentAsString)) {
+			return false;
+		}
+
+		CharLiteralExpr character = new CharLiteralExpr(argumentAsString);
+		var replacement = new MethodCallExpr(scope, METHOD_APPEND, NodeList.nodeList(character));
+
+		return tryReplace(methodCall, replacement);
+	}
+
+	private static boolean isSupportedType(Expression expression) {
+		if (expression == null) {
+			return false;
+		}
+		String typeName = expression.calculateResolvedType().describe();
+		return SUPPORTED_TYPES.contains(typeName);
+	}
+
+	private static boolean couldBeCharacter(String argumentAsString) {
+		return StringEscapeUtils.unescapeJava(argumentAsString).length() != 1;
+	}
+
+}
---
+++ java/src/test/java/eu/solven/cleanthat/engine/java/refactorer/cases/do_not_format_me/TestAppendCharacterWithCharCases.java
@@ -0,0 +1,123 @@
+package eu.solven.cleanthat.engine.java.refactorer.cases.do_not_format_me;
+
+import eu.solven.cleanthat.engine.java.refactorer.annotations.CompareMethods;
+import eu.solven.cleanthat.engine.java.refactorer.annotations.UnmodifiedMethod;
+import eu.solven.cleanthat.engine.java.refactorer.meta.IJavaparserAstMutator;
+import eu.solven.cleanthat.engine.java.refactorer.mutators.AppendCharacterWithChar;
+import eu.solven.cleanthat.engine.java.refactorer.test.AJavaparserRefactorerCases;
+
+import java.io.CharArrayWriter;
+import java.io.StringWriter;
+
+public class TestAppendCharacterWithCharCases extends AJavaparserRefactorerCases {
+
+    @Override
+    public IJavaparserAstMutator getTransformer() {
+        return new AppendCharacterWithChar();
+    }
+
+    private static final class CustomStringBuilder {
+        private CustomStringBuilder append(String string) {
+            return this;
+        }
+    }
+
+    @UnmodifiedMethod
+    public static class CaseCustomStringBuilder {
+        public Object pre() {
+            return new CustomStringBuilder().append("a");
+        }
+    }
+
+    @CompareMethods
+    public static class CaseCharArrayWriter {
+        public Object pre() {
+            return new CharArrayWriter().append("a");
+        }
+
+        public Object post() {
+            return new CharArrayWriter().append('a');
+        }
+    }
+
+    @CompareMethods
+    public static class CaseStringWriter {
+        public Object pre() {
+            return new StringWriter().append("a");
+        }
+
+        public Object post() {
+            return new StringWriter().append('a');
+        }
+    }
+
+    @CompareMethods
+    public static class CaseStringBuffer {
+        public Object pre() {
+            return new StringBuffer().append("a");
+        }
+
+        public Object post() {
+            return new StringBuffer().append('a');
+        }
+    }
+
+    @CompareMethods
+    public static class CaseStringBuilder {
+        public Object pre() {
+            return new StringBuilder().append("a");
+        }
+
+        public Object post() {
+            return new StringBuilder().append('a');
+        }
+    }
+
+    @CompareMethods
+    public static class CaseCharChain {
+        public Object pre() {
+            return new StringBuilder().append("a").append("b");
+        }
+
+        public Object post() {
+            return new StringBuilder().append('a').append('b');
+        }
+    }
+
+    @UnmodifiedMethod
+    public static class CaseString {
+        public Object pre() {
+            return new StringBuilder().append("aa");
+        }
+    }
+
+    @UnmodifiedMethod
+    public static class CaseStringChain {
+        public Object pre() {
+            return new StringBuilder().append("aa").append("bb");
+        }
+    }
+
+    @CompareMethods
+    public static class CaseMixedChain {
+        public Object pre() {
+            return new StringBuilder().append("a").append("bb");
+        }
+
+        public Object post() {
+            return new StringBuilder().append('a').append("bb");
+        }
+    }
+
+    @CompareMethods
+    public static class CaseMixedChainFlipped {
+        public Object pre() {
+            return new StringBuilder().append("aa").append("b");
+        }
+
+        public Object post() {
+            return new StringBuilder().append("aa").append('b');
+        }
+    }
+
+}
